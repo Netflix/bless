fd81deab6b00c5ddc5c4576f37316c26e6c5682c
Vivian Ho <vho@lyft.com>
2016-08-03 10:16:22 -0700

    made bless lambda compatibile for creating a host certificate

--- bless/request/bless_request.py
+++ bless/request/bless_request.py
@@ -9,6 +9,7 @@ from marshmallow import Schema, fields, post_load, ValidationError
 
 # man 8 useradd
 USERNAME_PATTERN = re.compile('[a-z_][a-z0-9_-]*[$]?\Z')
+HOSTNAME_PATTERN = re.compile('[a-z0-9_.-]+')
 
 
 def validate_ip(ip):
@@ -25,22 +26,36 @@ def validate_user(user):
         raise ValidationError('Username contains invalid characters')
 
 
+def validate_host(hostname):
+    if len(hostname) > 64:
+        raise ValidationError('Hostname is too long')
+    if HOSTNAME_PATTERN.match(hostname) is None:
+        raise ValidationError('Hostname contains invalid characters')
+
+
 class BlessSchema(Schema):
     bastion_ip = fields.Str(validate=validate_ip)
     bastion_user = fields.Str(validate=validate_user)
     bastion_user_ip = fields.Str(validate=validate_ip)
     command = fields.Str()
     public_key_to_sign = fields.Str()
-    remote_username = fields.Str(validate=validate_user)
 
     @post_load
     def make_bless_request(self, data):
         return BlessRequest(**data)
 
 
+class BlessUserSchema(BlessSchema):
+    remote_username = fields.Str(validate=validate_user)
+
+
+class BlessHostSchema(BlessSchema):
+    remote_hostnames = fields.List(fields.Str())
+
+
 class BlessRequest:
     def __init__(self, bastion_ip, bastion_user, bastion_user_ip, command, public_key_to_sign,
-                 remote_username):
+                 remote_username=None, remote_hostnames=None):
         """
         A BlessRequest must have the following key value pairs to be valid.
         :param bastion_ip: The source IP where the SSH connection will be initiated from.  This is
@@ -51,14 +66,20 @@ class BlessRequest:
         :param public_key_to_sign: The id_rsa.pub that will be used in the SSH request.  This is
         enforced in the issued certificate.
         :param remote_username: The username on the remote server that will be used in the SSH
+        :param remote_hostnames: A list of hostnames on the server for which the certificate is valid
         request.  This is enforced in the issued certificate.
         """
+
+        if remote_username is None and remote_hostnames is None:
+            raise ValidationError('Username or hostnames must be provided')
+
         self.bastion_ip = bastion_ip
         self.bastion_user = bastion_user
         self.bastion_user_ip = bastion_user_ip
         self.command = command
         self.public_key_to_sign = public_key_to_sign
         self.remote_username = remote_username
+        self.remote_hostnames = remote_hostnames
 
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
---
+++ tests/aws_lambda/test_bless_lambda_host.py
@@ -0,0 +1,32 @@
+import os
+import pytest
+
+from bless.aws_lambda.bless_lambda import lambda_handler
+from bless.ssh.certificates.ssh_certificate_builder import SSHCertificateType
+from tests.ssh.vectors import EXAMPLE_RSA_PUBLIC_KEY, RSA_CA_PRIVATE_KEY_PASSWORD, \
+    EXAMPLE_ED25519_PUBLIC_KEY
+
+
+class Context(object):
+    aws_request_id = 'bogus aws_request_id'
+    invoked_function_arn = 'bogus invoked_function_arn'
+
+
+VALID_TEST_REQUEST = {
+    "remote_hostnames": ["example.com", "example1.com"],
+    "public_key_to_sign": EXAMPLE_RSA_PUBLIC_KEY,
+    "command": "ssh user@server",
+    "bastion_ip": "127.0.0.1",
+    "bastion_user": "user",
+    "bastion_user_ip": "127.0.0.1"
+}
+
+os.environ['AWS_REGION'] = 'us-west-2'
+
+
+def test_basic_local_request():
+    cert = lambda_handler(VALID_TEST_REQUEST, context=Context,
+                          ca_private_key_password=RSA_CA_PRIVATE_KEY_PASSWORD,
+                          entropy_check=False, certificate_type=SSHCertificateType.HOST,
+                          config_file=os.path.join(os.path.dirname(__file__), 'bless-test.cfg'))
+    assert cert.startswith('ssh-rsa-cert-v01@openssh.com ')
