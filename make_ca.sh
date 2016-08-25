86749965325f578bb9ee003cb0eb62bf4e678016
Chris Steipp <csteipp@lyft.com>
2016-08-25 10:24:42 -0700

    Update make_ca.sh for multiregion

--- make_ca.sh
+++ make_ca.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-
 PASS=`openssl rand -base64 30`
 ssh-keygen -t rsa -b 4096 -f bless-ca-key -N $PASS -C "SSH CA Key"
-echo -n "Enter your your key-id: "
-read KEYID
-ENCPASS=`aws kms encrypt --key-id $KEYID --plaintext $PASS --query CiphertextBlob`
-echo "Your encrypted password: $ENCPASS"
+
+# IAD
+ENCPASS=`aws kms encrypt --region us-east-1 --key-id 'arn:aws:kms:us-east-1:036177710368:key/0a66690a-63e4-4e8a-94f5-1412c90321ba' --plaintext $PASS --query CiphertextBlob`
+echo "Your encrypted password (IAD): $ENCPASS"
+
+#us-west-2
+ENCPASS=`aws kms encrypt --region us-west-2 --key-id 'arn:aws:kms:us-west-2:036177710368:key/9c53e079-44b6-473e-a497-fa7f39f50016' --plaintext $PASS --query CiphertextBlob`
+echo "Your encrypted password (us-west-2): $ENCPASS"
