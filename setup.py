17e5336987779aa094abdc7a33312d03d7f63b40
Russell Lewis <russelll@netflix.com>
2017-02-24 12:47:07 -0800

    Moving pinned packages out of setup.py and into requirements.txt.

--- setup.py
+++ setup.py
@@ -18,32 +18,20 @@ setup(
     license=about["__license__"],
     packages=find_packages(exclude=["test*"]),
     install_requires=[
-        'boto3==1.3.1',
-        'botocore==1.4.37',
-        'cffi==1.7.0',
-        'cryptography==1.4',
-        'docutils==0.12',
-        'enum34==1.1.6',
-        'futures==3.0.5',
-        'idna==2.1',
-        'ipaddress==1.0.16',
-        'jmespath==0.9.0',
-        'marshmallow==2.9.0',
-        'pyasn1==0.1.9',
-        'pycparser==2.14',
-        'python-dateutil==2.5.3',
-        'six==1.10.0',
-        'kmsauth==0.1.7'
+        'boto3',
+        'botocore',
+        'cffi',
+        'cryptography',
+        'ipaddress',
+        'marshmallow',
+        'kmsauth'
     ],
     extras_require={
         'tests': [
-            'coverage==4.1',
-            'flake8==2.6.2',
-            'mccabe==0.5.0',
-            'pep8==1.7.0',
-            'py==1.4.31',
-            'pyflakes==1.2.3',
-            'pytest==2.9.2'
+            'coverage',
+            'flake8',
+            'pyflakes',
+            'pytest'
         ]
     }
 )
